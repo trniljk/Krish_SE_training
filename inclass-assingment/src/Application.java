e1c31aa53dc22c8ead95b15b2f56f68e749962a3
Ishara Kulathilaka <trniljk@gmail.com>
2021-01-29 21:36:57 +0530

    in class assingment

---
+++ inclass-assingment/src/Application.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	    System.out.println("Any Word");
+
+	    String inputWord = scanner.nextLine();
+		
+		String wordLowercase = inputWord.toLowerCase();
+		
+		char charArry [] = wordLowercase.toCharArray();
+		
+		int sumOfValues =0;
+	
+		for(int i =0 ; i<charArry.length;i++) {
+			if(charArry[i]==32)
+				break;
+			
+			if((charArry[i]>=97) && (charArry[i]<=122))
+				sumOfValues += (charArry[i]-96);
+		}
+		if (sumOfValues!=0) {
+			System.out.println("Value of entered letters : "+sumOfValues);
+			
+		} else {
+			System.out.println("Enter word not contain letters");
+
+		}		
+	}
+
+}
